66e016e1808312ff2bac9e39b021239d6c937198
Chanakya74 <chanakyamahato00@gmail.com>
2025-04-30 16:04:20 +0530

    first commit modifed

--- First.java
+++ First.java
@@ -2,6 +2,12 @@ class First
 {
 public static void main (String [] args)
 {
+<<<<<<< HEAD
 System.out.println("Hello world");
 }
 }
+=======
+System.out.print("Hello World!");
+}
+}
+>>>>>>> d9810c8 (first commit)
